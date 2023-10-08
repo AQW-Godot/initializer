08fbe7535751111e2403d659b6e2c182c4080b31
John Marlo Lapiz <lazuee.dev@gmail.com>
2023-10-08 15:19:47 +0000

    update

--- src/index.gd
+++ src/index.gd
@@ -10,14 +10,12 @@ var _request_nodes : Dictionary = {}
 var _initialize_error : bool = false
 var _load_scenes : Array[Dictionary] = [
 	{
-		"name": "Menu",
-		"url": _base_url.format({ "path": "menu" }),
-		"scene": "res://menu/index.tscn"
+		"name": "menu",
+		"url": _base_url.format({ "path": "menu" })
 	},
 	{
-		"name": "Client",
-		"url": _base_url.format({ "path": "client" }),
-		"scene": "res://client/index.tscn"
+		"name": "client",
+		"url": _base_url.format({ "path": "client" })
 	}
 ]
 
@@ -29,7 +27,7 @@ func _ready():
 	while not _main.is_node_ready(): await _tree.process_frame
 
 	for load_scene in _load_scenes:
-		load_scene_from_url(load_scene.name, load_scene.url, load_scene.scene)
+		load_scene_from_url(load_scene.name, load_scene.url)
 
 	await get_tree().create_timer(1.0).timeout
 	while not _request_nodes.is_empty(): await _tree.process_frame
@@ -44,8 +42,7 @@ func _ready():
 	await current_scene.tree_exited
 	_tree.current_scene = _main
 
-func load_scene_from_url(_name: String, _url: String, _scene: String) -> AwaitSignal:
-	_name = _name.to_pascal_case()
+func load_scene_from_url(_name: String, _url: String) -> AwaitSignal:
 	var _await = AwaitSignal.new()
 	var _request : HTTPRequest
 	if not _url in _request_nodes:
@@ -57,52 +54,54 @@ func load_scene_from_url(_name: String, _url: String, _scene: String) -> AwaitSi
 		_request_nodes[_url] = _request
 	else: _request = _request_nodes[_url]
 
+	var regex = RegEx.new()
+	regex.compile("^(?>(?<protocol>[a-zA-Z]+)://)?(?<domain>[a-zA-Z0-9.\\-_]*)?(?>:(?<port>\\d{1,5}))?(?<path>[a-zA-Z0-9_\\-/%]+)?/(?<file>[a-zA-Z0-9_\\-.%]+)")
+	var url_reg = regex.search(_url)
+	var file_dir = "user://temp/{dir}"
+	var file_name = ""
+	var file_ext = ""
+	if url_reg:
+		file_dir = file_dir.format({ "dir": url_reg.get_string("path") if not url_reg.get_string("path")[0] == "/" else url_reg.get_string("path").erase(0) })
+		file_name = url_reg.get_string("file").get_slice(".", 0)
+		file_ext = url_reg.get_string("file").get_slice(".", 1)
+	var file_path = "{dir}/{name}.{ext}".format({ "dir": file_dir, "name": file_name, "ext": file_ext })
+
+	if DirAccess.dir_exists_absolute(file_dir): DirAccess.remove_absolute(file_path)
+	DirAccess.make_dir_recursive_absolute(file_dir)
+
 	Callable(func():
-		var uuid = UUID.v4().substr(0, 8)
-		var file_name = _url.get_file().get_slice(".", 0)
-		var file_ext = _url.get_file().get_slice(".", 1)
-		var file_path = "user://temp/{name}-{uuid}.{ext}".format({ "uuid": uuid, "name": file_name, "ext": file_ext })
-
-		if not DirAccess.dir_exists_absolute(file_path.get_base_dir()):
-			DirAccess.make_dir_recursive_absolute(file_path.get_base_dir())
-
 		var _label = Label.new()
 		_label.name = _name
-		_label.text = "Initializing ..."
-		_label.add_theme_font_size_override("font_size", 35)
+		_label.text = "Loading..."
+		_label.add_theme_font_size_override("font_size", 28)
 		_label_container.add_child.call_deferred(_label)
 		while not _label.is_node_ready(): await _tree.process_frame
 
-		Callable(func():
-			_await.update.connect(func(progress, _downloaded_bytes, _body_size):
-				_label.text = "Initializing %s %s%%" % [_name, progress]
-			)
+		_await.update.connect(func(progress, _downloaded_bytes, _body_size):
+			_label.text = "Loading %s %s%%" % [_name, progress]
+		)
 
-			_await.complete.connect(func():
-				if _await.error_message:
-					_label.text = "Initializing %s failed!" % [_name]
-					_initialize_error = true
-				else:
-					var _resource = ResourceLoader.load(_scene, "PackedScene")
-					if _resource:
-						var _scene_ = _resource.instantiate()
-						_scene_.name = _name
-						_main.add_child.call_deferred(_scene_)
-						while not _scene_.is_node_ready(): await _tree.process_frame
-				_request_nodes[_url].queue_free()
-				await _request_nodes[_url].tree_exited
-				_request_nodes.erase(_url)
-			)
-		).call()
-
-		_await.update.emit(0.0, 0.0, 0.0)
-
-		_request.download_file = file_path
-		_request.request_completed.connect(func(result: int, response_code: int, _headers: PackedStringArray, _body: PackedByteArray):
-			if result == HTTPRequest.RESULT_SUCCESS && response_code == HTTPClient.RESPONSE_OK:
-				ProjectSettings.load_resource_pack(file_path)
+		_await.complete.connect(func():
+			if _await.error_message:
 				DirAccess.remove_absolute(file_path)
+				_label.text = "Loading %s failed!" % [_name]
+				_initialize_error = true
 			else:
+				ProjectSettings.load_resource_pack(file_path, true)
+				var _resource = ResourceLoader.load("res://{scene}.tscn".format({ "scene": _name.to_lower() }), "PackedScene")
+				if _resource:
+					var _scene_ = _resource.instantiate()
+					_scene_.name = _name.to_pascal_case()
+					_main.add_child.call_deferred(_scene_)
+					while not _scene_.is_node_ready(): await _tree.process_frame
+
+			_request_nodes[_url].queue_free()
+			await _request_nodes[_url].tree_exited
+			_request_nodes.erase(_url)
+		)
+
+		_request.request_completed.connect(func(result: int, response_code: int, _headers: PackedStringArray, _body: PackedByteArray):
+			if result != HTTPRequest.RESULT_SUCCESS && response_code != HTTPClient.RESPONSE_OK:
 				var error_message = "The HTTP request for the asset pack did not succeed. "
 				match result:
 					HTTPRequest.RESULT_CHUNKED_BODY_SIZE_MISMATCH:
@@ -159,12 +158,12 @@ func load_scene_from_url(_name: String, _url: String, _scene: String) -> AwaitSi
 			_await.complete.emit()
 		).call_deferred()
 
-		Callable(func():
-			var error = _request.request(_url)
-			if error != OK:
-				var error_message = "An error occurred while making the HTTP request: %d." % error
-				_await.error_message = error_message
-			).call_deferred()
+		_await.update.emit(0.0, 0.0, 0.0)
+		_request.download_file = file_path
+		var error = _request.request(_url)
+		if error != OK:
+			var error_message = "An error occurred while making the HTTP request: %d." % error
+			_await.error_message = error_message
 	).call()
 
 	return _await
